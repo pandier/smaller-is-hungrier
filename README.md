1743940b71414ac1d8c80da576bacee228af16f1
pandier <firegox@email.cz>
2024-07-21 22:47:03 +0200

    feat: Choose between health and food as source

--- README.md
+++ README.md
@@ -13,12 +13,19 @@ This is definitely an original idea that didn't come from [OwengeJuiceTV](https:
 ```hocon
 # Defines the range in which the resulted scale will be.
 # 
-# The high property defines the scale when the player has 20 hunger
-# and the low property defines the scale when the player reaches 0 hunger.
+# The high property defines the scale when the player has 20 hunger/health
+# and the low property defines the scale when the player reaches 0 hunger/health.
 # 
 # You can also set the low value higher than the high value to make the player bigger when hungrier!
 range {
   high=1.0
   low=0.45
 }
+
+# Defines the source which will be used to calculate the scale.
+# 
+# Possible values:
+#  - food = uses the players food level with high at 20
+#  - health = uses the players health with high at 20 (players will grow on excess health)
+source=food
 ```
--- src/main/kotlin/io/github/pandier/smallerhungrier/SmallerHungrier.kt
+++ src/main/kotlin/io/github/pandier/smallerhungrier/SmallerHungrier.kt
@@ -4,7 +4,10 @@ import com.google.inject.Inject
 import org.apache.logging.log4j.Logger
 import org.spongepowered.api.Server
 import org.spongepowered.api.config.DefaultConfig
+import org.spongepowered.api.data.DataTransactionResult
+import org.spongepowered.api.data.Key
 import org.spongepowered.api.data.Keys
+import org.spongepowered.api.data.value.Value
 import org.spongepowered.api.entity.attribute.type.AttributeTypes
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.entity.living.player.server.ServerPlayer
@@ -16,10 +19,14 @@ import org.spongepowered.api.event.network.ServerSideConnectionEvent
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
+import org.spongepowered.configurate.serialize.ScalarSerializer
+import org.spongepowered.configurate.serialize.SerializationException
 import org.spongepowered.plugin.PluginContainer
 import org.spongepowered.plugin.builtin.jvm.Plugin
 import java.io.IOException
+import java.lang.reflect.Type
 import java.nio.file.Path
+import java.util.function.Predicate
 
 @Suppress("unused")
 @Plugin("smallerhungrier")
@@ -31,6 +38,11 @@ class SmallerHungrier @Inject constructor(
 ) {
     private val configLoader = HoconConfigurationLoader.builder()
         .path(configPath)
+        .defaultOptions { options ->
+            options.serializers { builder ->
+                builder.register(Configuration.Source.Serializer())
+            }
+        }
         .build()
     private lateinit var config: Configuration
 
@@ -47,60 +59,85 @@ class SmallerHungrier @Inject constructor(
     }
 
     /**
-     * Calculates the scale based on the food level of the specified [player].
-     * The range in which the scale will be mapped can be configured
-     * in the plugin's [Configuration].
-     */
-    private fun calculateScale(player: Player): Double =
-        calculateScale(player.foodLevel().get())
-
-    /**
-     * Calculates the scale based on the specified [foodLevel].
-     * The range in which the scale will be mapped can be configured
-     * in the plugin's [Configuration].
-     */
-    private fun calculateScale(foodLevel: Int): Double =
-        config.range.map(foodLevel / 20.0)
-
-    /**
      * Updates the player's scale attribute based on the specified [scale].
-     * Uses [calculateScale] if scale is not specified.
+     * Uses [Configuration.scaleForPlayer] if scale is not specified.
      */
-    private fun updateScale(player: Player, scale: Double = calculateScale(player)) {
+    private fun updateScale(player: Player, scale: Double = config.scaleForPlayer(player)) {
         val scaleAttribute = player.attribute(AttributeTypes.GENERIC_SCALE)
             .orElseThrow { IllegalStateException("Could not find 'generic.scale' attribute for player '${player.name()}'") }
         scaleAttribute.setBaseValue(scale)
     }
 
     @Listener
-    fun handleJoin(event: ServerSideConnectionEvent.Join) {
+    private fun handleJoin(event: ServerSideConnectionEvent.Join) {
         updateScale(event.player())
     }
 
     @Listener
-    fun handlePlayerSpawn(event: RespawnPlayerEvent.Post) {
+    private fun handlePlayerSpawn(event: RespawnPlayerEvent.Post) {
         updateScale(event.entity())
     }
 
     @Listener
-    fun handleFoodLevelChange(event: ChangeDataHolderEvent.ValueChange) {
+    private fun handleFoodLevelChange(event: ChangeDataHolderEvent.ValueChange) {
         val player = event.targetHolder() as? ServerPlayer ?: return
-        val value = event.endResult().successfulValue(Keys.FOOD_LEVEL).orElse(null) ?: return
-        updateScale(player, calculateScale(value.get()))
+        val scale = config.scaleForTransaction(event.endResult())
+        if (scale != null) {
+            updateScale(player, scale)
+        }
     }
 
     @ConfigSerializable
     data class Configuration(
         @field:Comment(
+            "Defines the source which will be used to calculate the scale.\n"
+                    + "\n"
+                    + "Possible values:\n"
+                    + "  - food = uses the players food level with high at 20\n"
+                    + "  - health = uses the players health with high at 20 (players will grow on excess health)"
+        )
+        val source: Source = Source.FOOD,
+
+        @field:Comment(
             "Defines the range in which the resulted scale will be.\n"
                     + "\n"
-                    + "The high property defines the scale when the player has 20 hunger\n"
-                    + "and the low property defines the scale when the player reaches 0 hunger.\n"
+                    + "The high property defines the scale when the player has 20 hunger/health\n"
+                    + "and the low property defines the scale when the player reaches 0 hunger/health.\n"
                     + "\n"
                     + "You can also set the low value higher than the high value to make the player bigger when hungrier!"
         )
         val range: Range = Range()
     ) {
+        enum class Source(val function: Function<*>) {
+            FOOD(Function(Keys.FOOD_LEVEL) { it / 20.0 }),
+            HEALTH(Function(Keys.HEALTH) { it / 20.0 });
+
+            class Function<E>(
+                private val key: Key<Value<E>>,
+                private val mapper: (E) -> Double
+            ) {
+                fun invokeTransaction(transaction: DataTransactionResult): Double? {
+                    val value = transaction.successfulValue(key).orElse(null) ?: return null
+                    return mapper(value.get())
+                }
+
+                fun invokePlayer(player: Player): Double =
+                    mapper.invoke(player.require(key))
+            }
+
+            class Serializer : ScalarSerializer<Source>(Source::class.java) {
+                override fun deserialize(type: Type, obj: Any): Source {
+                    val name = obj.toString().uppercase()
+                    return Source.entries.find { it.name == name }
+                        ?: throw SerializationException("Unknown source: $obj")
+                }
+
+                override fun serialize(obj: Source, typeSupported: Predicate<Class<*>>): Any {
+                    return obj.name.lowercase()
+                }
+            }
+        }
+
         @ConfigSerializable
         data class Range(
             val low: Double = 0.45,
@@ -109,5 +146,22 @@ class SmallerHungrier @Inject constructor(
             fun map(value: Double): Double =
                 low + (high - low) * value
         }
+
+        /**
+         * Calculates scale for the specified [player]
+         * based on the user specified settings.
+         */
+        fun scaleForPlayer(player: Player): Double =
+            source.function.invokePlayer(player).let(range::map)
+
+        /**
+         * Calculates scale for the specified [transaction]
+         * based on the user specified settings.
+         *
+         * Returns null if the transaction doesn't contain
+         * the required successful value.
+         */
+        fun scaleForTransaction(transaction: DataTransactionResult): Double? =
+            source.function.invokeTransaction(transaction)?.let(range::map)
     }
 }
